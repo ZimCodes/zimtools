8aa0e95454b5623f1fc663561e3a751c06e3269f
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-09-27 22:24:14 -0500

    adds favicon

--- comps/header.js
+++ comps/header.js
@@ -10,6 +10,7 @@ const Header = props => <div>
         <meta name="description" content="Zim's command-line options configurator. Make using the command-line interface easier with this productive web tool."/>
         <meta name="keywords" content="wget,ffmpeg,youtube-dl,zim,wizard,options,configurator,commandline"/>
         <title>ZimTools</title>
+        <link rel="icon" href="/favicon.ico"/>
     </Head>
     {props.children}
 </div>;
