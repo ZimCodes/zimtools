67452092afde5d8d3eec46c2e15bdfc8e9def477
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2020-10-31 22:55:43 -0500

    changes hompage cards from circles to ovals

--- comps/wiz.js
+++ comps/wiz.js
@@ -5,7 +5,7 @@ const Wiz = (props) => (
     <div className="col-lg-3">
         <Link href={props.link}>
             <a className={`${styles.noUnderline} `}>
-                <div className={`card ${styles.card} text-center rounded-circle ${styles.shadow}`}>
+                <div className={`card ${styles.card} text-center rounded-pill ${styles.shadow}`}>
                     <h3 className={`${styles.cardHeader} rounded-pill card-header font-weight-bold ${props.headStyle}`}>{props.title}</h3>
                     <div className="card-body">
                         <div className={"card-text text-dark text-center"}>{props.about}</div>
